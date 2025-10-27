70dceda8b9bdb0cb7e1f605b76c0920d2d9d8eaa
elinaviksne <elina@gmail.com>
2025-10-27 10:57:56 +0200

    cenas

---
+++ ticket-price.js
@@ -0,0 +1,13 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (isStudent==true){
+ {console.log("biļetes cena:5 eiro")
+    
+} 
+    if ( visitorAge< 6) {
+    console.log("Biļetes cena:parvelti")
+} else {
+    console.log("Biļetes cena:10 eiro")
+}
+}
