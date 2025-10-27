b2b8fd580a9ed2f6fda3785492c8934bdd48591a
elinaviksne <elina@gmail.com>
2025-10-27 11:30:27 +0200

    internet - store.js

---
+++ internet-store.js
@@ -0,0 +1,22 @@
+let precuSaraksts =(
+    {
+        nosaukums : "austiņas",
+        kategorija : "elektronika",
+        cena : 30,
+        daudzums : 10
+    },
+    {
+        nosaukums : "kleita",
+        kategorija: "apģerbs",
+        cena: 15,
+        daudzums: 20
+    },
+    {
+        
+        nosaukums : "blūze",
+        kategorija: "apģerbs",
+        cena: 5,
+        daudzums: 10
+    }
+
+)
